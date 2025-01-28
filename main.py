cca8d438b8f8bd7665d4d21bc2ad8796396952a4
__zisis__ <georgezisis@gmail.com>
2025-01-28 13:11:32 +0200

    Add the residual model
    Main now tests the model's output.

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from tictactoe import TicTacToe
-import numpy as np
+import warnings
+# There's an annoying warning when using CuDNN and this removes it
+warnings.filterwarnings("ignore", ".*Applied workaround*.",)
 
+import numpy as np
+import torch
+
+from tictactoe import TicTacToe
+from model import ResNet
+
+# Initialize game
 game = TicTacToe()
 state = game.get_initial_state()
 player = 1
-print(game.get_valid_moves_indices(state))
+
+# Get random first action
 action = np.random.choice(game.get_valid_moves_indices(state))
 state = game.get_next_state(state, action, player)
 game.render(state)
 player = player * -1
 
-while True:
-    reward = game.get_reward(state, 1)
-    if reward is not None:
-        print(f"Game ends with reward: {reward}!")
-        break
-    best_score, action = game.minimax(state, 0, player==1)
-    state = game.get_next_state(state, action, player)
-    print(game.get_valid_moves_indices(state))
-    game.render(state)
-    player = player * -1
+# Check model
+encoded_state = game.get_encoded_state(state)
+print(encoded_state)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"The device we use is: {device}")
+
+tensor_state = torch.tensor(encoded_state, device=device).unsqueeze(0)
+print(tensor_state)
+
+model = ResNet(game, device=device, input_channels=3, filters=64, res_blocks=9)
+
+policy, value = model(tensor_state)
+
+value = value.item()
+policy = torch.softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
+
+print(f"The value of current state is: {value}")
+print(f"The policy of the current state is: {policy}")
